2eca4d7f57cce71891cdf61758346d84918a18db
KRochaS <karinerocha.s@hotmail.com>
2020-07-14 20:47:38 -0300

    Form e HTML

--- Criando Apps Web com Angular 8 e Ionic 4/eshop/src/app/pages/account/login/login.page.ts
+++ Criando Apps Web com Angular 8 e Ionic 4/eshop/src/app/pages/account/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { LoadingController, ToastController, NavController } from '@ionic/angular';
 import { DataService } from 'src/app/data.service';
 
@@ -18,9 +18,23 @@ export class LoginPage implements OnInit {
       private toastCtrl: ToastController,
       private navCtrl: NavController,
       private service: DataService
-  ) { }
+  ) {
+
+    this.form = this.fb.group({
+        username: ['', Validators.compose([Validators.required])],
+        password: ['', Validators.compose([
+            Validators.minLength(6),
+            Validators.maxLength(20),
+            Validators.required
+        ])]
+    })
+   }
 
   ngOnInit() {
+  }
+
+  toggleHide() {
+      this.hide = !this.hide;
   }
 
 }
